68a05ca7bba7e13cd559fdcfb6764c5080e07c38
Marc Català <reskit@gmail.com>
2015-06-03 11:32:49 +0100

    Canvi de nom del tema

--- settings.php
+++ settings.php
@@ -23,7 +23,7 @@
  * For full information about creating Moodle themes, see:
  * http://docs.moodle.org/dev/Themes_2.0
  *
- * @package   theme_clean
+ * @package   theme_ioc_clean
  * @copyright 2013 Moodle, moodle.org
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -33,17 +33,17 @@ defined('MOODLE_INTERNAL') || die;
 if ($ADMIN->fulltree) {
 
     // Invert Navbar to dark background.
-    $name = 'theme_clean/invert';
-    $title = get_string('invert', 'theme_clean');
-    $description = get_string('invertdesc', 'theme_clean');
+    $name = 'theme_ioc_clean/invert';
+    $title = get_string('invert', 'theme_ioc_clean');
+    $description = get_string('invertdesc', 'theme_ioc_clean');
     $setting = new admin_setting_configcheckbox($name, $title, $description, 0);
     $setting->set_updatedcallback('theme_reset_all_caches');
     $settings->add($setting);
 
     // Logo file setting.
-    $name = 'theme_clean/logo';
-    $title = get_string('logo','theme_clean');
-    $description = get_string('logodesc', 'theme_clean');
+    $name = 'theme_ioc_clean/logo';
+    $title = get_string('logo','theme_ioc_clean');
+    $description = get_string('logodesc', 'theme_ioc_clean');
     $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo');
     $setting->set_updatedcallback('theme_reset_all_caches');
     $settings->add($setting);
@@ -65,18 +65,18 @@ if ($ADMIN->fulltree) {
     $settings->add($setting);
 
     // Custom CSS file.
-    $name = 'theme_clean/customcss';
-    $title = get_string('customcss', 'theme_clean');
-    $description = get_string('customcssdesc', 'theme_clean');
+    $name = 'theme_ioc_clean/customcss';
+    $title = get_string('customcss', 'theme_ioc_clean');
+    $description = get_string('customcssdesc', 'theme_ioc_clean');
     $default = '';
     $setting = new admin_setting_configtextarea($name, $title, $description, $default);
     $setting->set_updatedcallback('theme_reset_all_caches');
     $settings->add($setting);
 
     // Footnote setting.
-    $name = 'theme_clean/footnote';
-    $title = get_string('footnote', 'theme_clean');
-    $description = get_string('footnotedesc', 'theme_clean');
+    $name = 'theme_ioc_clean/footnote';
+    $title = get_string('footnote', 'theme_ioc_clean');
+    $description = get_string('footnotedesc', 'theme_ioc_clean');
     $default = '';
     $setting = new admin_setting_confightmleditor($name, $title, $description, $default);
     $setting->set_updatedcallback('theme_reset_all_caches');
