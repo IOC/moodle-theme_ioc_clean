7531773f7304d47f638f0fe0089cbcd7417ce921
Marc Català <reskit@gmail.com>
2015-06-03 11:32:49 +0100

    Canvi de nom del tema

--- lang/en/theme_ioc_clean.php
+++ lang/en/theme_ioc_clean.php
@@ -15,9 +15,9 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'theme_clean', language 'en'
+ * Strings for component 'theme_ioc_clean', language 'en'
  *
- * @package   theme_clean
+ * @package   theme_ioc_clean
  * @copyright 2013 Moodle, moodle.org
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -25,12 +25,12 @@
 $string['choosereadme'] = '
 <div class="clearfix">
 <div class="well">
-<h2>Clean</h2>
-<p><img class=img-polaroid src="clean/pix/screenshot.jpg" /></p>
+<h2>IOC Clean</h2>
+<p><img class=img-polaroid src="ioc_clean/pix/screenshot.jpg" /></p>
 </div>
 <div class="well">
 <h3>About</h3>
-<p>Clean is a modified Moodle bootstrap theme which inherits styles and renderers from its parent theme.</p>
+<p>Clean_ioc is a modified Moodle clean theme which inherits styles and renderers from its parent theme.</p>
 <h3>Parents</h3>
 <p>This theme is based upon the Bootstrap theme, which was created for Moodle 2.5, with the help of:<br>
 Stuart Lamour, Mark Aberdour, Paul Hibbitts, Mary Evans.</p>
@@ -45,7 +45,7 @@ Website: <a href="http://www.basbrands.nl">www.basbrands.nl</a>
 <p><a href="clean/README.txt">How to copy and customise this theme.</a></p>
 </div></div>';
 
-$string['configtitle'] = 'Clean';
+$string['configtitle'] = 'IOC Clean';
 
 $string['customcss'] = 'Custom CSS';
 $string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
@@ -59,7 +59,7 @@ $string['invertdesc'] = 'Swaps text and background for the navbar at the top of 
 $string['logo'] = 'Logo';
 $string['logodesc'] = 'The logo is only displayed in the header of the front page and login page.<br /> If the height of your logo is more than 75px add div.logo {height: 100px;} to the Custom CSS box below, amending accordingly if the height is other than 100px. If a logo is not added here, the logo in Appearance > Logos will be used.';
 
-$string['pluginname'] = 'Clean';
+$string['pluginname'] = 'IOC Clean';
 
 $string['region-side-post'] = 'Right';
 $string['region-side-pre'] = 'Left';
