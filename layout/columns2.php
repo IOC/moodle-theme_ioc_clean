386da15215d9883dd0b07ce81e2293f38b731333
Marc Català <mcatala@itteria.cat>
2015-06-15 12:41:20 +0200

    Afegit el mode de proves

--- layout/columns2.php
+++ layout/columns2.php
@@ -38,7 +38,8 @@ echo $OUTPUT->doctype() ?>
 <html <?php echo $OUTPUT->htmlattributes(); ?>>
 <head>
     <title><?php echo $OUTPUT->page_title(); ?></title>
-    <link rel="shortcut icon" href="<?php echo $OUTPUT->favicon(); ?>" />
+    <?php $imgname = (empty($CFG->local_testing_mode)?'favicon':'favicon_proves'); ?>
+    <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url($imgname, 'theme')?>" />
     <?php echo $OUTPUT->standard_head_html() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -47,11 +48,12 @@ echo $OUTPUT->doctype() ?>
 
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
 
-<header role="banner" class="navbar navbar-fixed-top<?php echo $html->navbarclass ?> moodle-has-zindex">
+<header role="banner" class="navbar navbar-fixed-top<?php echo $html->navbarclass ?> moodle-has-zindex <?php echo (empty($CFG->local_testing_mode) ? '' : ' proves') ?>">
     <nav role="navigation" class="navbar-inner">
         <div class="container-fluid">
             <a class="brand" href="<?php echo $CFG->wwwroot;?>">
-                <img src="<?php echo $OUTPUT->pix_url('ioc_logo', 'theme')?>" />
+                <?php $imgname = (empty($CFG->local_testing_mode))?'ioc_logo':'ioc_proves'; ?>
+                <img src="<?php echo $OUTPUT->pix_url($imgname, 'theme')?>" />
             </a>
             <?php echo $OUTPUT->navbar_button(); ?>
             <?php echo $OUTPUT->user_menu(); ?>
